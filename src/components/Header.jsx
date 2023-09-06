a215a71c82a558d20172e78da782a4d7b98f91fc
nadra10 <125341985+nadra10@users.noreply.github.com>
2023-09-06 16:31:13 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <div className='logo'>
+                Movies App
+            </div>
+            
+        </div>
+    );
+}
+
+export default Header;
